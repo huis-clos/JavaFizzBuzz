588f6d3dd6bcae0245b1c4261f89dd6f7d871156
Colleen Toth <tothcol@pdx.edu>
2014-07-09 20:09:16 -0700

    Second commit. Begins Testing and fleshing out main.

--- src/main/java/edu/pdx/cd410J/tothcol/FizzBuzz.java
+++ src/main/java/edu/pdx/cd410J/tothcol/FizzBuzz.java
@@ -4,4 +4,12 @@ package edu.pdx.cd410J.tothcol;
  * Created by ctizzle on 7/9/14.
  */
 public class FizzBuzz {
+
+    public static String fizzbuzz(int number) {
+        return "1";
+    }
+
+    public static void count(int number) {
+        for (int i = 1; i < number: i++)
+    }
 }
--- src/test/java/edu/pdx/cd410J/tothcol/FizzBuzzTest.java
+++ src/test/java/edu/pdx/cd410J/tothcol/FizzBuzzTest.java
@@ -1,7 +1,17 @@
 package edu.pdx.cd410J.tothcol;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by ctizzle on 7/9/14.
  */
 public class FizzBuzzTest {
+
+    @Test
+    public void testPrintOne(){
+        assertEquals("FizzBuzz of one", "1", FizzBuzz.fizzbuzz(1));
+    }
+
 }
